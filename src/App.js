3c081a06420e74bf27e290e33e83eb9ef55fe85b
trungduong0810 <trungduong0810@gmail.com>
2024-01-23 10:53:59 +0700

    update file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
 // import Picsum_Photos from "./components/chuong5_useEffect/API_picsum_photo/PicsumPhotos";
 // import Hacknews_Api from "./components/chuong5_useEffect/hacknews/Hacknews_Api";
-import HackNewsWithReducer from "./components/chuong5_useEffect/hacknewWithReducer/HackNewsWithReducer";
+// import HackNewsWithReducer from "./components/chuong5_useEffect/hacknewWithReducer/HackNewsWithReducer";
 // import Count from "./components/Chuong3_state/useReducer/Count";
 // import Todo from "./components/Chuong3_state/useReducer/todo";
-// import Tictactoe from "./components/Chuong3_state/game/Tictactoe ";
+import Tictactoe from "./components/Chuong3_state/game/Tictactoe ";
 // import ToggleState from "./components/Chuong3_state/uesState/toggle_state";
 // import Youtube from "./components/Chuong2_component_rendering/youtubeList";
 
 const App = () => {
   return (
     <div>
-    <HackNewsWithReducer></HackNewsWithReducer>
+    {/* <HackNewsWithReducer></HackNewsWithReducer> */}
     {/* <Hacknews_Api></Hacknews_Api> */}
       {/* <Picsum_Photos></Picsum_Photos> */}
       {/* <Todo></Todo> */}
       {/* <Count></Count> */}
       {/* <Youtube></Youtube>
       <ToggleState></ToggleState> */}
-      {/* <Tictactoe></Tictactoe> */}
+      <Tictactoe></Tictactoe>
     </div>
   );
 };
